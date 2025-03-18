e74f80523bfaf0e2dbce43aaa498f8feef2f7ff5
Parker Vannah <parker.vannah.71@protonmail.com>
2025-03-18 14:08:37 -0500

    Try to fix file tree

---
+++ src/main/java/dev/velocity71/Graphy/Main.java
@@ -0,0 +1,52 @@
+package dev.velocity71.Graphy;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Main class for the Graphy application. Contains the control scene.
+ */
+public class Main extends Application {
+
+	// Variable to contain the information in the config file, see src/main/resources/config.properties.
+	private static Properties config = new Properties();
+
+    @Override
+    public void start(Stage controlStage) throws Exception {
+
+    	// Must load the config file before anything else.
+    	loadConfig();
+
+     	// Load data for the control scene from the control.FXML file located at src/main/resources/fxml/control.fxm.
+        Parent control = FXMLLoader.load(getClass().getClassLoader().getResource(config.getProperty("control.fxml.path")));
+
+
+        controlStage.setScene(new Scene(control));
+        controlStage.setTitle("Graphy");
+        controlStage.show();
+        System.out.println("Graphy started successfully.");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static void loadConfig() throws IOException {
+            try {
+                final InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties"); // Locate config file.
+                if (input == null) {
+                    throw new IOException("Unable to find config.properties resource."); // More specific error message for debugging.
+                }
+                config.load(input);
+            } catch (final IOException e) {
+                throw new IOException("Error loading config.properties", e); // Wrap original exception for better context in stack trace.
+            }
+        }
+}
